fb7e5050e2c78171d33d3f764f625994a0a93aa0
mateoroca <mateojoserocaclemntis@gmail.com>
2023-04-22 16:22:20 -0300

    new logic back-end with 'eval()'

--- Primer-cuatrimestre/frontend-diagnostic/calculadora/index.js
+++ Primer-cuatrimestre/frontend-diagnostic/calculadora/index.js
@@ -6,99 +6,33 @@ const buttonEqual = document.querySelector("#equal");
 const buttonDelete = document.querySelector("#delete");
 const result = document.querySelector("#result");
 
-
-
-let ActualOperation = '';
-
-let PreviusOperation = '';
-
-let operation = undefined;
-
+let display = '';
 
 buttonsNumber.forEach(function(button){
-  button.addEventListener('click',function(){
-      addNumber(button.innerText);
+  button.addEventListener('click',()=>{
+   display = `${result.value}${button.value}` ;
+    result.value = display;
   })
 })
 
 buttonsNumberOpereitors.forEach(function(button){
-  button.addEventListener('click',function(){
-     selectOperation(button.innerText);
-     result.value = button.value;
+  button.addEventListener('click',()=>{
+    display = `${result.value}${button.value}` ;
+    result.value = display;
   })
 })
 
-buttonEqual.addEventListener('click',function(){
-      calculate();
-      UpdateDisplay();
-      
-  }
-)
 
-buttonDelete.addEventListener('click',function (){
-  clear();
-  UpdateDisplay();
+  buttonDelete.addEventListener('click',()=>{
+    result.value = '';
+
+  })
+
+
+buttonEqual.addEventListener('click',()=>{
+    result.value = eval(display);
+
 })
 
-
-function selectOperation(op){
-  if(ActualOperation ==='' )return;
-  if(PreviusOperation != '' ){
-       calculate();
-  }
-
-  operation = op.toString();
-  PreviusOperation = ActualOperation;
-  ActualOperation = '';
-
-}
-
-function addNumber(num){
-    ActualOperation = ActualOperation.toString()+num.toString();
-    UpdateDisplay();
-}
-
-function UpdateDisplay(){
-  result.value = ActualOperation;
-}
-
-
-function clear(){
-  ActualOperation  = '';
-  PreviusOperation = '';
-  Operation = '';
-}
-
-
-
-
-function calculate(){
-  let res;
-  const previus = parseFloat(PreviusOperation);
-  const actual =  parseFloat(ActualOperation);
-
-  if(isNaN(previus)||isNaN(actual))return;
-  switch(operation){
-    case '+':
-      res = previus + actual;
-      break;
-    case '-':
-      res = previus - actual;
-      break;
-    case '*':
-      res = previus * actual;
-      break;
-    case'/':
-        res = previus / actual; 
-      break;
-      default :
-          return;        
-  } 
- 
-  ActualOperation = res;
-  operation = undefined;
-  PreviusOperation = '';
-    
-} 
 
 
